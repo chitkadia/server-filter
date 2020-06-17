898811a7a2eef61aefa024a8b21bfff705733bc5
Tarak Kadiya <chitkadia@gmail.com>
2020-06-17 14:44:57 +0530

    Added static json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
 
+import { LocaldataService } from './services/localdata.service';
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'infibeam';
+    title = 'infibeam';
+    jsonData: any = [];
+
+    constructor(
+        public readLocalData: LocaldataService
+    ) {}
+
+    async ngOnInit() {
+        console.log(this.readLocalData.jsonData);
+    }
 }
